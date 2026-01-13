15166dd062567cded5e483f770e32bffef12dbf3
mugtaba <49446523+mugtaba-subahi@users.noreply.github.com>
2026-01-13 13:10:14 +0000

    new progress bar feature completed. ready to test and merge

--- components/Timer.tsx
+++ components/Timer.tsx
@@ -1,5 +1,5 @@
 import { useAtomValue } from 'jotai';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import Animated, { useAnimatedStyle, withTiming } from 'react-native-reanimated';
 
 import ProgressBar from './ProgressBar';
@@ -55,7 +55,7 @@ const styles = StyleSheet.create({
   text: {
     textAlign: 'center',
     fontSize: TEXT.sizeSmall,
-    marginBottom: Platform.OS === 'android' ? 3 : 6,
+    marginBottom: 3,
     color: COLORS.textSecondary,
   },
   timer: {
