847657c20710e5ecc1a3e91a23abbdbb51053465
mugtaba <49446523+mugtaba-subahi@users.noreply.github.com>
2026-01-21 17:12:25 +0000

    remove masjid onboarding

--- components/Masjid.tsx
+++ components/Masjid.tsx
@@ -1,14 +1,6 @@
-import * as Haptics from 'expo-haptics';
-import { useAtomValue } from 'jotai';
-import { Pressable, StyleSheet } from 'react-native';
-import Animated from 'react-native-reanimated';
+import { StyleSheet, View } from 'react-native';
 
 import Icon from '@/assets/icons/masjid.svg';
-import { useAnimationScale } from '@/hooks/useAnimation';
-import { overlayAtom } from '@/stores/overlay';
-import { showSettingsSheet } from '@/stores/ui';
-
-const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
 
 type MasjidProps = {
   width?: number;
@@ -16,30 +8,18 @@ type MasjidProps = {
 };
 
 export default function Masjid({ height = 55, width = 55 }: MasjidProps) {
-  const AnimScale = useAnimationScale(1);
-  const overlay = useAtomValue(overlayAtom);
-
-  const handlePress = () => {
-    if (overlay.isOn) return;
-
-    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
-    showSettingsSheet();
-  };
-
   return (
-    <AnimatedPressable
-      onPress={handlePress}
-      onPressIn={() => !overlay.isOn && AnimScale.animate(0.9)}
-      onPressOut={() => AnimScale.animate(1)}
-      style={AnimScale.style}
-      hitSlop={15}
-      disabled={overlay.isOn}>
+    <View style={styles.container}>
       <Icon style={styles.icon} height={height} width={width} />
-    </AnimatedPressable>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   icon: {
     shadowColor: '#EF9C29',
     shadowOffset: { width: 5, height: 5 },
