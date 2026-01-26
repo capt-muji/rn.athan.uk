df26817da64ac141356290f387c96d4b1a46db3f
mugtaba <49446523+mugtaba-subahi@users.noreply.github.com>
2026-01-26 07:33:16 +0000

    added bottom sheet alert

--- components/BottomSheetSound.tsx
+++ components/BottomSheetSound.tsx
@@ -79,7 +79,7 @@ export default function BottomSheetSound() {
         <View style={styles.header}>
           <View>
             <Text style={styles.title}>Select Athan</Text>
-            <Text style={styles.subtitle}>Choose your notification sound</Text>
+            <Text style={styles.subtitle}>Prayer time notification sound</Text>
           </View>
           <View style={styles.headerIcon}>
             <IconView type={Icon.SPEAKER} size={16} color="rgba(165, 180, 252, 0.8)" />
